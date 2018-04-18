ee141b8fa5cfecafd375b26d1a4d3a6715136902
winiceo <winiceo@163.com>
2018-04-18 11:40:12 +0800

    abc

--- app/router.js
+++ app/router.js
@@ -6,8 +6,10 @@ module.exports = app => {
     const { router, controller, middleware } = app;
 
     const {
-        account, balance, wallet, miner, email, test,home
+        account, balance, wallet, miner, email, test, home
     } = controller;
+
+    const apipre = '/api';
 
     const tokenCheck = middleware.tokenCheck();
 
@@ -19,31 +21,31 @@ module.exports = app => {
     /**
      * 用户相关
      */
-    router.post('/account/login', account.login);// 用户登录
-    router.post('/account/register', account.register);// 用户注册
-    router.post('/account/forget', account.forget);// 修改密码
-    router.post('/account/changePassword', tokenCheck, account.updatePass);// 修改密码
-    router.post('/account/balances', tokenCheck, balance.total);// 修改密码
+    router.post(apipre + '/account/login', account.login);// 用户登录
+    router.post(apipre + '/account/register', account.register);// 用户注册
+    router.post(apipre + '/account/forget', account.forget);// 修改密码
+    router.post(apipre + '/account/changePassword', tokenCheck, account.updatePass);// 修改密码
+    router.post(apipre + '/account/balances', tokenCheck, balance.total);// 修改密码
 
 
     /**
      * 钱包相关
      */
-    router.post('/wallet/assets', tokenCheck, wallet.assets);
-    router.post('/wallet/withdraw', tokenCheck, wallet.withdraw);
-    router.post('/wallet/withdraw/history', tokenCheck, wallet.withdrawHistory);
+    router.post(apipre + '/wallet/assets', tokenCheck, wallet.assets);
+    router.post(apipre + '/wallet/withdraw', tokenCheck, wallet.withdraw);
+    router.post(apipre + '/wallet/withdraw/history', tokenCheck, wallet.withdrawHistory);
 
 
     /**
      * miner相关
      */
-    router.post('/miner', tokenCheck, miner.index);
-    router.post('/miner/:miner_id', tokenCheck, miner.detail);
+    router.post(apipre + '/miner', tokenCheck, miner.index);
+    router.post(apipre + '/miner/:miner_id', tokenCheck, miner.detail);
 
     /**
      * 邮箱相关
      */
-    router.post('/email/captcha', email.sendCaptcha);
+    router.post(apipre + '/email/captcha', email.sendCaptcha);
 
     /**
      * 测试相关
